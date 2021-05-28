ddf944cc95a81b0c50e4e5a66504be1b416845ea
Torben Teepe <t.teepe@tum.de>
2021-05-28 13:52:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DCNv2',
+    version='2.0.0',
+    description='Deformable Convolutional Networks v2 (DCNv2) with Pytorch 1.8+ & JIT Compilation',
+    author='Torben Teepe',
+    author_email='torben@tee.pe',
+    url='https://gitlab.lrz.de/tteepe/DCNv2',
+    packages=find_packages(),
+)
